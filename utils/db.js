9fbc84d676906012cb126a18d3a1e7e621aa70ec
gonzalopriv9-byte <gonzalo.priv9@gmail.com>
2026-02-19 17:53:27 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,8 @@
-const { Pool } = require("pg");
+const { createClient } = require("@supabase/supabase-js");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
 
-module.exports = { pool };
+module.exports = { supabase };
