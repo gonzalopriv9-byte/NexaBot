960ff6e7892cffaef3445981b28f6bb08db857dc
gonzalopriv92-code <gonzalo.priv.9.2@gmail.com>
2026-02-23 17:32:10 +0100

    Add default values to Terraform variables for one-click deploy
    Added default values for compartment_id, subnet_id, image_id, and ssh_public_key variables.

--- variables.tf
+++ variables.tf
@@ -1,25 +1,29 @@
 variable "region" {
   description = "Región de Oracle Cloud"
-  default     = "eu-madrid-1"
+  default = "eu-madrid-1"
 }
 
 variable "compartment_id" {
   description = "OCID del compartment"
-  type        = string
+  type = string
+  default = "ocid1.tenancy.oc1..aaaaaaaa4jazq73q5ytxp4itu6wu2kivwhhfrsw3rx6354oanusrxq7nhs4a"
 }
 
 variable "subnet_id" {
   description = "OCID de la subnet"
-  type        = string
+  type = string
+  default = "ocid1.subnet.oc1.eu-madrid-1.aaaaaaaavk2zocaiixjfspbsuyqxo55vwntbi3j7zhwobdgslcts5hgxm2ta"
 }
 
 variable "image_id" {
-  description = "OCID de la imagen"
-  type        = string
+  description = "OCID de la imagen (Ubuntu 22.04 free-tier)"
+  type = string
+  default = "ocid1.image.oc1.eu-madrid-1.aaaaaaaaci6hvfgdj73ukkmpyb3ilc5alg5qpyc3rknmlqfj5qwsv3emglfa"
 }
 
 variable "ssh_public_key" {
   description = "Tu clave SSH pública"
-  type        = string
-  sensitive   = true
+  type = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDX5xKL2qPmhV0XYXQKmvzKJ4Zkx8Qk1H5Zy7KX5Z7X2Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z7Z5Z5 nexabot@oracle"
+  sensitive = true
 }
