8fff2c64d878b56ae04d4b231df8cd46972f8186
gonzalopriv92-code <gonzalo.priv.9.2@gmail.com>
2026-02-23 17:11:31 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "oci" {
+  region = var.region
+}
+
+# Ejemplo simple - Compute Instance para ejecutar Nexa-Bot
+resource "oci_core_instance" "nexa_bot" {
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  compartment_id      = var.compartment_id
+  
+  display_name = "nexa-bot-instance"
+  shape        = "VM.Standard.E2.1.Micro"  # Free tier elegible
+  
+  create_vnic_details {
+    subnet_id                 = var.subnet_id
+    assign_public_ip          = true
+    hostname_label            = "nexa-bot"
+    private_ip                = "10.0.0.2"
+  }
+
+  source_details {
+    source_type             = "IMAGE"
+    source_id               = var.image_id  # Ubuntu 22.04 o similar
+    boot_volume_size_in_gbs = 50
+  }
+
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+    user_data           = base64encode(file("${path.module}/user-data.sh"))
+  }
+
+  freeform_tags = {
+    "app" = "nexa-bot"
+  }
+}
+
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.compartment_id
+}
+
+output "instance_public_ip" {
+  value       = oci_core_instance.nexa_bot.public_ip
+  description = "Public IP de la instancia"
+}
